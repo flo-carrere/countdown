4dcd892a03c8cf78f8ac2bd1e169adb7d776c3c7
flo-carrere <carrere.florian@gmail.com>
2026-02-01 14:01:20 +0100

    formatting applied

--- main.cpp
+++ main.cpp
@@ -1,17 +1,16 @@
-#include <iostream>
 #include <algorithm>
+#include <iostream>
 #include <map>
-#include <vector>
 #include <random>
 #include <time.h>
+#include <vector>
 
 using namespace std;
 
-int solve( vector<int>& numbers, int target, int nearest );
-int solve_impl( int number1, int number2, const vector<int> numbers, int target, int nearest);
+int solve(vector<int> &numbers, int target, int nearest);
+int solve_impl(int number1, int number2, const vector<int> numbers, int target, int nearest);
 
-int main( int argc, char** argv )
-{
+int main(int argc, char **argv) {
     srand(time(NULL));
 
     const vector<int> allNumbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 25, 50, 75, 100};
@@ -26,10 +25,8 @@ int main( int argc, char** argv )
     cout << "Mode manuel? (y:1, n:0)" << endl;
     cin >> manualMode;
 
-    if( manualMode )
-    {
-        for( int i = 1 ; i < howMany+1 ; ++i )
-        {
+    if (manualMode) {
+        for (int i = 1; i < howMany + 1; ++i) {
             int number;
             cout << "Number " << i << "? ";
             cin >> number;
@@ -44,17 +41,15 @@ int main( int argc, char** argv )
 
     do {
         // Choose a target to obtain or approach the most
-        if( !manualMode )
-        {
+        if (!manualMode) {
             target = rand() % (highLimit - lowLimit) + lowLimit;
             numbers.clear();
         }
 
         // Choose numbers
         cout << "Numbers are: ";
-        for( int i = 0 ; i < howMany ; ++i )
-        {
-            if( !manualMode )
+        for (int i = 0; i < howMany; ++i) {
+            if (!manualMode)
                 numbers.push_back(allNumbers[rand() % allNumbers.size()]);
             cout << numbers[i] << " ";
         }
@@ -63,76 +58,68 @@ int main( int argc, char** argv )
         cout << "Target is: " << target << endl;
 
         // Exercise
-        int nearesult = solve( numbers, target, numbers[0] );
+        int nearesult = solve(numbers, target, numbers[0]);
 
-        if( nearesult == target )
+        if (nearesult == target)
             cout << "Le compte est bon!" << endl << endl;
         else
             cout << nearesult << endl << endl;
 
-    } while( getchar() != ' ' );
+    } while (getchar() != ' ');
 
     return 0;
 }
 
-int solve( vector<int>& numbers, int target, int nearest )
-{
+int solve(vector<int> &numbers, int target, int nearest) {
     int result = numbers[0];
 
-    for( size_t i = 0 ; i < numbers.size() ; ++i )
-    {
-        for( size_t j = i + 1 ; j < numbers.size() ; ++j )
-        {
+    for (size_t i = 0; i < numbers.size(); ++i) {
+        for (size_t j = i + 1; j < numbers.size(); ++j) {
             vector<int> copyNumbers = numbers;
-            copyNumbers.erase(copyNumbers.begin()+i);
-            copyNumbers.erase(copyNumbers.begin()+j-1);
-            result = solve_impl( numbers[i], numbers[j], copyNumbers, target, nearest);
-            if( abs(target-result) < abs(target-nearest) )
-            {
+            copyNumbers.erase(copyNumbers.begin() + i);
+            copyNumbers.erase(copyNumbers.begin() + j - 1);
+            result = solve_impl(numbers[i], numbers[j], copyNumbers, target, nearest);
+            if (abs(target - result) < abs(target - nearest)) {
                 nearest = result;
 
-                cout << "New nearest: " << nearest << " Numbers are: " ;
-                for( size_t i = 0 ; i < numbers.size() ; ++i )
+                cout << "New nearest: " << nearest << " Numbers are: ";
+                for (size_t i = 0; i < numbers.size(); ++i)
                     cout << numbers[i] << " ";
                 cout << endl;
             }
         }
     }
 
-    if( abs(target-result) < abs(target-nearest) )
+    if (abs(target - result) < abs(target - nearest))
         nearest = result;
 
     return nearest;
 }
 
-int solve_impl( int number1, int number2, vector<int> numbers, int target, int nearest )
-{
+int solve_impl(int number1, int number2, vector<int> numbers, int target, int nearest) {
     int result = 0;
 
     // Intermediate result is already nearer
-    if( abs(target-number1) < abs(target-nearest) )
+    if (abs(target - number1) < abs(target - nearest))
         nearest = number1;
 
-    if( abs(target-number2) < abs(target-nearest) )
+    if (abs(target - number2) < abs(target - nearest))
         nearest = number2;
 
     // Addition
-    numbers.push_back(number1+number2);
+    numbers.push_back(number1 + number2);
     result = solve(numbers, target, nearest);
-    if( result != nearest )
-    {
+    if (result != nearest) {
         cout << number1 << '+' << number2 << " = " << number1 + number2 << endl;
         nearest = result;
     }
     numbers.pop_back();
 
     // Product
-    if( (number1 != 1) && (number2 != 1) )
-    {
-        numbers.push_back(number1*number2);
+    if ((number1 != 1) && (number2 != 1)) {
+        numbers.push_back(number1 * number2);
         result = solve(numbers, target, nearest);
-        if( result != nearest )
-        {
+        if (result != nearest) {
             cout << number1 << 'x' << number2 << " = " << number1 * number2 << endl;
             nearest = result;
         }
@@ -140,23 +127,18 @@ int solve_impl( int number1, int number2, vector<int> numbers, int target, int n
     }
 
     // Substraction
-    if( number1 > number2 )
-    {
-        numbers.push_back(number1-number2);
+    if (number1 > number2) {
+        numbers.push_back(number1 - number2);
         result = solve(numbers, target, nearest);
-        if( result != nearest )
-        {
+        if (result != nearest) {
             cout << number1 << '-' << number2 << " = " << number1 - number2 << endl;
             nearest = result;
         }
         numbers.pop_back();
-    }
-    else if( number2 > number1 )
-    {
-        numbers.push_back(number2-number1);
+    } else if (number2 > number1) {
+        numbers.push_back(number2 - number1);
         result = solve(numbers, target, nearest);
-        if( result != nearest )
-        {
+        if (result != nearest) {
             cout << number2 << '-' << number1 << " = " << number2 - number1 << endl;
             nearest = result;
         }
@@ -164,24 +146,20 @@ int solve_impl( int number1, int number2, vector<int> numbers, int target, int n
     }
 
     // Division
-    if( (number2 != 1) && ((number1 % number2) == 0) )
-    {
-        numbers.push_back(number1/number2);
+    if ((number2 != 1) && ((number1 % number2) == 0)) {
+        numbers.push_back(number1 / number2);
         result = solve(numbers, target, nearest);
-        if( result != nearest )
-        {
+        if (result != nearest) {
             cout << number1 << '/' << number2 << " = " << number1 / number2 << endl;
             nearest = result;
         }
         numbers.pop_back();
     }
 
-    if( (number1 != 1) && ((number2 % number1) == 0) )
-    {
-        numbers.push_back(number2/number1);
+    if ((number1 != 1) && ((number2 % number1) == 0)) {
+        numbers.push_back(number2 / number1);
         result = solve(numbers, target, nearest);
-        if( result != nearest )
-        {
+        if (result != nearest) {
             cout << number2 << '/' << number1 << " = " << number2 / number1 << endl;
             nearest = result;
         }
